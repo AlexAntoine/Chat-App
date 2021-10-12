16ecf097d19949682425af68dd01f8620a87eaae
AlexAntoine <antoinealex1234@gmial.com>
2021-10-12 01:37:27 -0400

    server is configured with socket.io

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
-const path = require('path')
+const path = require('path');
+const http = require('http');
 const express = require('express')
+const socketIo = require('socket.io');
 
-const app = express()
+const app = express();
+
+/* socket.io expect raw http server. Express does not give access to that hence the refactored code  */
+const server = http.createServer(app);
+const io = socketIo(server);
 
 const port = process.env.PORT || 3000
 const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Server is up on port ${port}!`)
 })
