942ea73511abc05748370c907c0d25f61a26ce98
AlexAntoine <antoinealex1234@gmial.com>
2021-10-18 00:58:37 -0400

    commit

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,3 +1,15 @@
-console.log('hit from chat.js');
 
-io();
+const socket =  io();
+
+document.querySelector('#increment').addEventListener('click', ()=>{
+    console.log('button was clicked');
+
+    socket.emit('increment');
+});
+
+socket.on('countUpdated', (count)=>{
+
+    console.log('count has been updated ', count);
+  
+   
+});
