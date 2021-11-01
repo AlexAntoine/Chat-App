e9e17b96f88c14a091a5d3a5a5f2856988083a22
AlexAntoine <anotoinealex1234@gmail.com>
2021-10-31 23:13:53 -0400

    added autoscroll feature

--- public/js/chat.js
+++ public/js/chat.js
@@ -18,6 +18,53 @@ const sidebarTemplate = document.querySelector('#sidebar-template').innerHTML;
 //Options
 const {username, room} = Qs.parse(location.search,{ ignoreQueryPrefix: true });
 
+const autoScroll = ()=>{
+    //New message element 
+    const $newMessage = $messages.lastElementChild
+
+    console.log('value of $newMessages ', $newMessage);
+
+    // Get the height of new message
+    const newMessageStyles = getComputedStyle($newMessage);
+
+    // console.log('newMessageStyles: ', newMessageStyles);
+
+    const newMessageMargin = parseInt(newMessageStyles.marginBottom);
+    console.log('newMessageMargin: ', newMessageMargin);
+
+    //something is wrong with this 
+    const newMessageHeight = $newMessage.offsetHeight + newMessageMargin;
+
+    console.log('newMessage offSetHeight: ',$newMessage.offsetHeight);
+    
+    //newMessageHeight is NaN
+    console.log('newMessageHeight: ', newMessageHeight);
+
+    //visible Height
+    const visibleHeight = $messages.offsetHeight;
+
+    console.log('visibleHeight: ', visibleHeight);
+
+    //Height of messages container
+    const containerheight = $messages.scrollHeight
+
+    //How far have i scrolled
+    const scrollOffset = $messages.scrollTop + visibleHeight;
+
+    console.log('scrollOfSet: ', scrollOffset);
+
+    if(containerheight - newMessageHeight <= scrollOffset){
+
+        console.log('if statment code reaced her ');
+
+        console.log('diff of containerHeight and newMessageHeight');
+
+        $messages.scrollTop = $messages.scrollHeight
+    }
+
+    // console.log(newMessageMargin);
+}
+
 $messageForm.addEventListener('submit',(response)=>{
     response.preventDefault();
 
@@ -47,6 +94,21 @@ $messageForm.addEventListener('submit',(response)=>{
 
 });
 
+socket.on('message', ({username, text, createdAt})=>{
+
+    // console.log(message);
+
+    const html = Mustache.render(messageTemplates,{
+        username,
+        text,
+        createdAt: moment(createdAt).format('h:mm a') 
+    });
+    
+    $messages.insertAdjacentHTML('beforeend', html);
+
+    autoScroll();
+});
+
 socket.on('locationMessage', (message)=>{
 
     console.log(message);
@@ -59,6 +121,8 @@ socket.on('locationMessage', (message)=>{
     });
 
     $location.insertAdjacentHTML('beforeend', html);
+
+    autoScroll();
 });
 
 socket.on('roomData',({room, users})=>{
@@ -73,19 +137,7 @@ socket.on('roomData',({room, users})=>{
     console.log(users);
 })
 
-socket.on('message', ({username, text, createdAt})=>{
 
-    // console.log(message);
-
-    const html = Mustache.render(messageTemplates,{
-        username,
-        text,
-        // message: text,
-        createdAt: moment(createdAt).format('h:mm a') 
-    });
-    
-    $messages.insertAdjacentHTML('beforeend', html);
-});
 
 $sendLocationButton.addEventListener('click',()=>{
    
